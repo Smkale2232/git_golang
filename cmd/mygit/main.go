19dd78f2547d6f28eccfb0cb9471fdecd3d01b30
Smkale2232 <Smkale2232@gmail.com>
2024-09-12 13:22:36 +0530

    #IC4

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
-
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
 		os.Exit(1)
 	}
-
 	switch command := os.Args[1]; command {
 	case "init":
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
@@ -20,14 +21,20 @@ func main() {
 				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
 			}
 		}
-
 		headFileContents := []byte("ref: refs/heads/main\n")
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
 			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
 		}
-
 		fmt.Println("Initialized git directory")
-
+	case "cat-file":
+		sha := os.Args[3]
+		path := fmt.Sprintf(".git/objects/%v/%v", sha[0:2], sha[2:])
+		file, _ := os.Open(path)
+		r, _ := zlib.NewReader(io.Reader(file))
+		s, _ := io.ReadAll(r)
+		parts := strings.Split(string(s), "\x00")
+		fmt.Print(parts[1])
+		r.Close()
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
