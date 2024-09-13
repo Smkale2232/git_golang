d09850627e096fb8a1574fbe30d11549b32695f6
Smkale2232 <Smkale2232@gmail.com>
2024-09-13 16:53:27 +0530

    #IC4

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
-	"strings"
+	"path/filepath"
 )
 
-// Usage: your_program.sh <command> <arg1> <arg2> ...
+// Usage: your_git.sh <command> <arg1> <arg2> ...
 func main() {
+	// fmt.Println("Logs from your program will appear here!")
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
 		os.Exit(1)
@@ -21,20 +25,67 @@ func main() {
 				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
 			}
 		}
-		headFileContents := []byte("ref: refs/heads/main\n")
+		headFileContents := []byte("ref: refs/heads/master\n")
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
 			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
 		}
 		fmt.Println("Initialized git directory")
 	case "cat-file":
-		sha := os.Args[3]
-		path := fmt.Sprintf(".git/objects/%v/%v", sha[0:2], sha[2:])
-		file, _ := os.Open(path)
-		r, _ := zlib.NewReader(io.Reader(file))
-		s, _ := io.ReadAll(r)
-		parts := strings.Split(string(s), "\x00")
-		fmt.Print(parts[1])
-		r.Close()
+		if len(os.Args) == 4 && os.Args[2] == "-p" {
+			sha := os.Args[3]
+			dir := sha[0:2]
+			filename := sha[2:]
+			path := filepath.Join(".git", "objects", dir, filename)
+			b, err := ioutil.ReadFile(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error in cat-file p command %s\n", err)
+				os.Exit(1)
+			}
+			buf := bytes.NewBuffer(b)
+			r, err := zlib.NewReader(buf)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error in cat-file p command zlib %s\n", err)
+				os.Exit(1)
+			}
+			defer r.Close()
+			bs, err := io.ReadAll(r)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error in cat-file p command find null char %s\n", err)
+				os.Exit(1)
+			}
+			found := false
+			for _, b := range bs {
+				if found {
+					fmt.Print(string(b))
+				} else {
+					found = b == 0
+				}
+			}
+		}
+	case "hash-object":
+		file, _ := os.ReadFile(os.Args[3])
+		stats, _ := os.Stat(os.Args[3])
+		content := string(file)
+		contentAndHeader := fmt.Sprintf("blob %d\x00%s", stats.Size(), content)
+		sha := (sha1.Sum([]byte(contentAndHeader)))
+		hash := fmt.Sprintf("%x", sha)
+		blobName := []rune(hash)
+		blobPath := ".git/objects/"
+		for i, v := range blobName {
+			blobPath += string(v)
+			if i == 1 {
+				blobPath += "/"
+			}
+		}
+		var buffer bytes.Buffer
+		z := zlib.NewWriter(&buffer)
+		z.Write([]byte(contentAndHeader))
+		z.Close()
+		os.MkdirAll(filepath.Dir(blobPath), os.ModePerm)
+		f, _ := os.Create(blobPath)
+		defer f.Close()
+		f.Write(buffer.Bytes())
+		fmt.Print(hash)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
